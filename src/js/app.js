8fb11150e81ec29357b447e24218311b4e03068a
lucrampro <lucienramassamy@gmail.com>
2018-08-19 12:03:16 +0200

    animation terminer

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 $('document').ready(function () {
 
 
+
+
     //    MENU DEROULANT
     $(".burger").click(function () {
 
@@ -20,7 +22,130 @@ $('document').ready(function () {
         }
         $(".burger").toggleClass("open");
     });
+    //    PAGE VOYAGE
+
+    TweenMax.to("h1", 1, {
+        delay: 0.3,
+        x: "0",
+        opacity: "1",
+        ease: Expo.easeInOut
+    });
+
+    TweenMax.to('.gostJaune', 1, {
+        delay: 0.6,
+        width: "80%",
+        ease: Expo.easeInOut,
+        onComplete: function () {
+
+            TweenMax.to("p", 0.3, {
+                opacity: "1",
+                onComplete: function () {
+
+                    TweenMax.to(".USA", 0.3, {
+
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+
+                    TweenMax.to(".POLY", 0.3, {
+
+                        delay: 0.1,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+
+                    TweenMax.to(".SUI", 0.3, {
+
+                        delay: 0.15,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+
+                    TweenMax.to(".UK", 0.3, {
+
+                        delay: 0.20,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+
+                    TweenMax.to(".ES", 0.3, {
+
+                        delay: 0.25,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+                    TweenMax.to(".POR", 0.3, {
+
+                        delay: 0.30,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+                    TweenMax.to(".PAK", 0.3, {
+
+                        delay: 0.35,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+                    TweenMax.to(".IND", 0.3, {
+
+                        delay: 0.40,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+                    TweenMax.to(".MALI", 0.3, {
+
+                        delay: 0.45,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7)
+                    });
+
+                    TweenMax.to(".MALA", 0.3, {
+
+                        delay: 0.50,
+                        css: {
+                            scale: 1
+                        },
+                        ease: Back.easeOut.config(1.7),
+                    });
+
+                    TweenMax.to(".holiday", 0.5, {
+                        delay: 0.45,
+                        x: "-50%",
+                        scale: 1,
+                        opacity: 1,
+                        ease: Bounce.easeOut
+                    });
 
 
 
+                }
+            });
+        }
+    });
 });
